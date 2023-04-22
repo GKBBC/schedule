5687b5c064302ea57425f1515de4c22fe19937db
Chelsey Kallman <2010144@student.goddardusd.com>
2023-04-21 22:56:25 -0500

    more mobile friendly. a litte.

--- script.js
+++ script.js
@@ -23,6 +23,7 @@ if (document.body.classList.contains("counter")) {
 }
 
 if (document.body.classList.contains("schedule")) {
+    //mover
     const hh = new Date().getHours();
     const mm = new Date().getMinutes();
     const total = (hh*60 + mm) - 360;
@@ -34,5 +35,63 @@ if (document.body.classList.contains("schedule")) {
         mover.style.top = mathed + "%";
     } else {
         mover.style.display = "none";
+    }
+    
+    //mobile view :]
+    let x = window.matchMedia("(max-width: 600px)");
+
+    if(x.matches) {
+        //check the date. hide alll columns. show 1st. show the day's. show the next day's. :)
+        const dd = new Date().getDate();
+        const columns = document.getElementById("schedule-grid").children;
+        const grid = document.getElementById("schedule-grid");
+
+        let column2 = [];
+        let column3 = [];
+        let column4 = [];
+        let column5 = [];
+        let column6 = [];
+        let column7 = [];
+
+        window.onload = function() {
+            for (let i = 0; i < columns.length; i++) {
+
+                let gridColumnStart = window.getComputedStyle(columns[i]).getPropertyValue("grid-column-start");
+
+                if(gridColumnStart == 2) {
+                    column2.push(columns[i]);
+                } else if(gridColumnStart == 3) {
+                    column3.push(columns[i]);
+                } else if(gridColumnStart == 4) {
+                    column4.push(columns[i]);
+                } else if(gridColumnStart == 5) {
+                    column5.push(columns[i]);
+                } else if(gridColumnStart == 6) {
+                    column6.push(columns[i]);
+                } else if(gridColumnStart == 7) {
+                    column7.push(columns[i]);
+                }
+            }
+
+            function hideColumn(columnName) {
+                for (let i = 0; i < columnName.length; i++) {
+                    columnName[i].style.display = "none";
+                }
+            }
+
+            function resizeColumn(columnName, percent) {
+                for (let i = 0; i < columnName.length; i++) {
+                    columnName[i].style.width = percent;
+                }
+            }
+
+            if (dd <= 25) {
+                hideColumn(column4);
+                hideColumn(column5);
+                hideColumn(column6);
+                hideColumn(column7);
+                grid.style.gridAutoColumns = ".5fr 1fr 1fr 0fr 0fr 0fr 0fr";
+            }
+        }
     }
 }
